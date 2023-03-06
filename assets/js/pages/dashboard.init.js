8349e2238a3a6cd75aab51e4d0c82a5580587f8c
Kelvin Njuguna <ozzaynetwork@gmail.com>
2023-03-06 14:38:20 +0300

    kelvin: the inital files

---
+++ assets/js/pages/dashboard.init.js
@@ -0,0 +1,130 @@
+// weekly comparison
+var options = {
+        chart: {
+            height: 360,
+            type: "bar",
+            stacked: !1,
+            toolbar: {
+                show: !1
+            },
+            zoom: {
+                enabled: !0
+            },
+
+        },
+        plotOptions: {
+            bar: {
+                horizontal: !1,
+                columnWidth: "40%",
+                // endingShape: "rounded"
+            }
+        },
+        dataLabels: {
+            enabled: !1,
+        },
+        stroke: { show: !0, width: 2, colors: ["transparent"] },
+
+        yaxis: {
+            labels: {
+                formatter: function(value) {
+                    // return "KES " + value;
+                    return numeral(value).format('0,0 a')
+                },
+                // formatter: function(val, index) {
+
+                //     return numeral(val).format('0,0')
+                // },
+
+
+
+            },
+            title: {
+                text: "Trips",
+            }
+        },
+        series: [{
+            name: "Fullfiled Orders",
+            data: [12, 36, 12, 45, 568, 14, 562, ]
+        }, {
+            name: "Canceled Orders",
+            data: [124, 12, 256, 256, 256, 24, 23]
+        }],
+        xaxis: {
+            categories: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        },
+        colors: ["#556ee6", "#f46a6a"],
+        legend: {
+            position: "bottom"
+        },
+        fill: {
+            opacity: 1
+        },
+
+        tooltip: {
+            custom: function({ series, seriesIndex, dataPointIndex, w }) {
+                var data = w.globals.initialSeries[seriesIndex].data[dataPointIndex];
+
+                return '<ul>' +
+                    '<li><b>Price</b>: ' + data.x + '</li>' +
+                    '<li><b>Number</b>: ' + data.y + '</li>' +
+                    '<li><b>Product</b>: \'' + data.product + '\'</li>' +
+                    '<li><b>Info</b>: \'' + data.info + '\'</li>' +
+                    '<li><b>Site</b>: \'' + data.site + '\'</li>' +
+                    '</ul>';
+            }
+        },
+
+        tooltip: {
+            y: {
+                formatter: function(value, { series, seriesIndex, dataPointIndex, w }) {
+                    return numeral(value).format('0,0') + " Liters"
+
+                }
+            }
+        },
+
+
+    },
+    chart = new ApexCharts(document.querySelector("#weekly-comparison"), options);
+chart.render();
+
+
+
+// collections graph
+var options = {
+        series: [{
+            name: "Total Collections",
+            data: [17023658, 29123456, 41458975, 67123654, 22123654, 43789654, 36789623, 52320365, 24023147, 18012586, 36036985, 48025820]
+        }, {
+            name: "Commision Revenue",
+            data: [13025856, 23025632, 20032145, 8021457, 13032568, 27456987, 18235897, 22235789, 10213214, 16365478, 24456987, 22123568]
+        }],
+        yaxis: {
+            labels: {
+                formatter: function(value) {
+                    // return "KES " + value;
+                    return numeral(value).format('0,0 a')
+                },
+                // formatter: function(val, index) {
+
+                //     return numeral(val).format('0,0')
+                // },
+
+
+
+            },
+            title: {
+                text: "Amount in KES",
+            }
+        },
+        chart: { height: 350, type: "area", toolbar: { show: !1 } },
+        colors: ["#34c38f", "#f46a6a"],
+        dataLabels: { enabled: !1 },
+        stroke: { curve: "smooth", width: 2 },
+        fill: { type: "gradient", gradient: { shadeIntensity: 1, inverseColors: !1, opacityFrom: .45, opacityTo: .05, stops: [20, 100, 100, 100] } },
+        xaxis: { categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"] },
+        markers: { size: 3, strokeWidth: 3, hover: { size: 4, sizeOffset: 2 } },
+        legend: { position: "top", horizontalAlign: "right" }
+    },
+    chart = new ApexCharts(document.querySelector("#collections-graph"), options);
+chart.render();
